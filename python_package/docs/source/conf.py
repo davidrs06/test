b830180352a0e1fc4a1cfec336a93d271cd4d4fd
David Romascano <david.romascano@epfl.ch>
2022-05-17 14:25:32 +0200

    Added files

--- python_package/docs/source/conf.py
+++ python_package/docs/source/conf.py
@@ -20,7 +20,7 @@ extensions = [
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
 ]
-autosummary_generate = True  # Turn on sphinx.ext.autosummary, from https://stackoverflow.com/questions/2701998/sphinx-autodoc-is-not-automatic-enough/62613202#62613202
+
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3/', None),
     'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
